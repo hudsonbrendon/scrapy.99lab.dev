6fe7b10da1f2078eb80db08394e4e19c50878765
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2025-04-18 10:30:33 -0300

    fix: update headers

--- main.py
+++ main.py
@@ -13,8 +13,13 @@ async def get_user_id(username: str) -> Dict[str, Any]:
     """Get user information from Instagram API"""
     url = f"https://i.instagram.com/api/v1/users/web_profile_info/?username={username}"
     headers = {
-        "User-Agent": "Mozilla/5.0",
-        "x-ig-app-id": "936619743392459"
+        # this is internal ID of an instegram backend app. It doesn't change often.
+        "x-ig-app-id": "936619743392459",
+        # use browser-like features
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
+        "Accept-Language": "en-US,en;q=0.9,ru;q=0.8",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept": "*/*",
     }
     try:
         response = await httpx.AsyncClient().get(url, headers=headers)
@@ -40,7 +45,13 @@ async def get_last_post_date(username: str) -> Optional[datetime]:
         "variables": json.dumps(variables)
     }
     headers = {
-        "User-Agent": "Mozilla/5.0"
+        # this is internal ID of an instegram backend app. It doesn't change often.
+        "x-ig-app-id": "936619743392459",
+        # use browser-like features
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
+        "Accept-Language": "en-US,en;q=0.9,ru;q=0.8",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept": "*/*",
     }
     
     try:
